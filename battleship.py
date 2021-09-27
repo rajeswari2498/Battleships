17655216ebbfa49450c3a3a6be992a9d7654a85e
rajeswari2498 <rajeswariyesasvini@msitprogram.net>
2021-09-27 12:10:55 +0530

    completed the implemetation of  createShip

--- battleship.py
+++ battleship.py
@@ -62,7 +62,14 @@ Parameters: int ; int
 Returns: 2D list of ints
 '''
 def emptyGrid(rows, cols):
-    return
+    grid = []
+    for i in range(rows):
+        list =[]
+        for j  in range(cols):
+            list.append(1)
+        grid.append(list)
+    return grid
+
 
 
 '''
@@ -71,7 +78,13 @@ Parameters: no parameters
 Returns: 2D list of ints
 '''
 def createShip():
-    return
+    row = random.randint(1,8)
+    col = random.randint(1,8)
+    ship = random.randint(0,1)
+    if ship == 0:
+        return[[row,col-1],[row,col],[row,col+1]]
+    else:
+        return[[row-1,col],[row,col],[row+1,col]]
 
 
 '''
@@ -268,6 +281,7 @@ def runSimulation(w, h):
 
 # This code runs the test cases to check your work
 if __name__ == "__main__":
+    test.testCreateShip()
 
     ## Finally, run the simulation to test it manually ##
     runSimulation(500, 500)
