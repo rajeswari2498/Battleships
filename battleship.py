6014f9a0e56c6b4d4c864117c0d7744f23ccb723
rajeswari2498 <rajeswariyesasvini@msitprogram.net>
2021-10-06 16:09:10 +0530

    Completed the implementation of runGameTurn

--- battleship.py
+++ battleship.py
@@ -366,7 +366,7 @@ def runSimulation(w, h):
 
 # This code runs the test cases to check your work
 if __name__ == "__main__":
-    test.testGetComputerGuess()
+    test.testUpdateBoard()
 
     ## Finally, run the simulation to test it manually ##
     runSimulation(500, 500)
